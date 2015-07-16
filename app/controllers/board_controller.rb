9195cfd2b3271d4d8ad3a5050086c069885a0a26
Jimmy Xu <jianmin.xu@usingnow.com>
2015-07-16 12:52:03 +0800

    create the admin on devise.

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,4 +1,7 @@
 class BoardController < ApplicationController
   def index
   end
+
+  def backend
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admins
   get 'board/index'
+  get 'board/backend'
+
+  get 'admins/dashboard', to: 'admins#dashboard', as: 'admin_root'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
